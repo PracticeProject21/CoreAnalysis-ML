c927430ac5b98f3cecd5ea31f6a25608026b39aa
Anatoly.s <tolik.frnk@yandex.ru>
2021-11-14 18:12:17 +0300

    fix some bugs and changed numerate of classes

--- ML_core/preprocessing/change_data.py
+++ ML_core/preprocessing/change_data.py
@@ -2,15 +2,15 @@ import utils
 import DateCategories
 
 # for photo_id in DateCategories.ultraviolet_photos:
-#     tools.change_segments(photo_id,
+#     utils.change_segments(photo_id,
 #                           'ultraviolet',
 #                           DateCategories.colors_for_ultraviolet)
 #
 #
-# for photo_id in DateCategories.daylight_photos:
-#     tools.change_segments(photo_id,
-#                           'daylight',
-#                           DateCategories.colors_for_daylight)
-#
+for photo_id in DateCategories.daylight_photos:
+    utils.change_segments(photo_id,
+                          'daylight',
+                           DateCategories.colors_for_daylight)
+
 # tools.show_colormap()
 
