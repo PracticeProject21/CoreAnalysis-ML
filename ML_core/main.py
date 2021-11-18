392c5ffe162035df2a91e7171002de9e1fd272ad
Anatoly.s <tolik.frnk@yandex.ru>
2021-11-18 19:11:27 +0300

    change import

--- ML_core/main.py
+++ ML_core/main.py
@@ -1,11 +1,11 @@
 import torch
-from tools import model_predict, text_generation
+from .tools import model_predict, text_generation
 
 
 name_model_day = ''
 name_model_ultra = 'DeepLabV3Plus_resnet18_ultraviolet.pth'
 
-DAY_model = torch.load(name_model_day)
+#DAY_model = torch.load(name_model_day)
 ULTRA_model = torch.load(name_model_ultra)
 
 def ML_part(PIL_image, label):
@@ -14,8 +14,9 @@ def ML_part(PIL_image, label):
         result = text_generation(predict_mask, label)
         return result
     else:
+        pass
         # label == 'daylight'
-        predict_mask = model_predict(DAY_model, PIL_image)
-        result = text_generation(predict_mask, label)
-        return result
+        #predict_mask = model_predict(DAY_model, PIL_image)
+        #result = text_generation(predict_mask, label)
+        #return result
 
